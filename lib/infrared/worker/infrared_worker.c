9711f04b8cd0bac9fcfc5ced489125d3d1c398b8
Akiva Cohen <Akivaecohen@gmail.com>
2025-01-13 10:14:49 -0500

    made new method changes

--- lib/infrared/worker/infrared_worker.c
+++ lib/infrared/worker/infrared_worker.c
@@ -605,12 +605,16 @@ void infrared_worker_tx_stop(InfraredWorker* instance) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 17940a2c (made new method changes)
     notification_message(instance->notification, &sequence_blink_stop);
 =======
     if(instance->tx_blink_enable) {
         notification_message(instance->notification, &message_blink_stop);
     }
 >>>>>>> 2e59eb13 (made new method changes)
+<<<<<<< HEAD
 =======
     notification_message(instance->notification, &sequence_blink_stop);
 >>>>>>> c9fcee34 (working)
@@ -622,6 +626,8 @@ void infrared_worker_tx_stop(InfraredWorker* instance) {
 =======
     notification_message(instance->notification, &sequence_blink_stop);
 >>>>>>> 0992febe (working)
+=======
+>>>>>>> 17940a2c (made new method changes)
 
     instance->signal.timings_cnt = 0;
     furi_check(furi_stream_buffer_reset(instance->stream) == FuriStatusOk);
